b87fc125dffe006ac01ebee46e9a14c190a0ddd0
Yashom Dighe <yashom7@gmail.com>
2022-09-22 09:26:22 -0400

    vel republisher publishes the forward velocity

---
+++ scripts/vel_republisher.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+
+import rospy
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Float64
+from math import sqrt
+
+class Vel_Republisher:
+    
+    def __init__(self) -> None:
+        self.repub_val = Float64()
+        self.odom_world  = Odometry()
+
+        self.vel_repub = rospy.Publisher('/car_1/ground_velocity', Float64, queue_size=10)
+        self.odom_sub = rospy.Subscriber('/car_1/ground_truth', Odometry, self.odom_callback, queue_size=10)
+
+    def odom_callback(self, odom):
+        vel = odom.twist.twist.linear
+        vel_odom = sqrt(vel.x**2 + vel.y**2)
+        self.vel_repub.publish(vel_odom)
+
+
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('vel_republisher')
+
+        Vel_Republisher()
+
+        rospy.spin()
+
+    except rospy.ROSInterruptException:
+        rospy.loginfo("vel_republisher node shutdown")    
